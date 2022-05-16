e146ae60d4bdf0272a4e120ac1c334dd1e514c79
alpinist-453 <alpinist_453@mail.ru>
2022-05-17 01:11:33 +0300

    готово

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,24 +1,24 @@
+
+const arrSliders = Array.from(document.querySelectorAll('.slider__item'));
 const sliderNext = document.querySelector('.slider__arrow_next');
 const sliderPrev = document.querySelector('.slider__arrow_prev');
-let arrSliders = Array.from(document.querySelectorAll('.slider__item'));
-let arrSave = [];
+const arrDot = Array.from(document.querySelectorAll('.slider__dot'));
+const sliderDots = document.querySelectorAll('.slider__item');
+console.log(sliderDots)
 
 sliderNext.onclick = next;
 sliderPrev.onclick = prev;
 
-
 function next() {
-	arrSave = arrSliders.splice(0, 1);
-	arrSave.forEach(el => { el.classList.remove('slider__item_active') });
-	arrSliders = arrSliders.concat(arrSave);
-	arrSliders[0].classList.add('slider__item_active');
+	indexActiv = arrSliders.findIndex(el => Array.from(el.classList).includes('slider__item_active'));
+	arrSliders[indexActiv].classList.remove('slider__item_active');
+	indexActiv = (indexActiv === arrSliders.length - 1) ? 0 : ++indexActiv;
+	arrSliders[indexActiv].classList.add('slider__item_active');
 }
 
 function prev() {
-	arrSave = arrSliders.splice(arrSliders.length - 1, 1);
-	arrSave.forEach(el => { el.class === '.item__menu item__menu__activ' });
-	const indexDelete = arrSliders.findIndex(el => Array.from(el.classList).includes('slider__item_active'));
-	arrSliders[indexDelete].classList.remove('slider__item_active');
-	arrSliders = arrSave.concat(arrSliders);
-	arrSliders[0].classList.add('slider__item_active');
+	indexActiv = arrSliders.findIndex(el => Array.from(el.classList).includes('slider__item_active'));
+	arrSliders[indexActiv].classList.remove('slider__item_active');
+	indexActiv = (indexActiv === 0) ? arrSliders.length - 1 : --indexActiv;
+	arrSliders[indexActiv].classList.add('slider__item_active');
 }
